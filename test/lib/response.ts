d047c641b671edb6d1b88e028f1d2c37922a7685
Chris Lemler <chris@lemler.me>
2021-04-13 23:17:27 -0700

    Added basic test cases for response package

--- test/lib/response.ts
+++ test/lib/response.ts
@@ -6,5 +6,31 @@ describe('lib/response package unit tests', () => {
         const code = 'DEADBEEF';
         const message = 'An error occured that involved beef';
         expect(code).to.equal(code);
-      });
+    });
+
+    it('sucess returns the correct statusCode', () => {
+      const statusCode = 201;
+      const resp = response.success(statusCode, {}, {key: 'value'});
+      expect(resp.statusCode).to.equal(statusCode);
+  });
+
+  it('sucess returns the correct headers', () => {
+    const headers = {'traceId': '1122334455'};
+    const statusCode = 201;
+    const resp = response.success(statusCode, headers, {key: 'value'});
+    expect(resp.headers.hasOwnProperty('traceId')).to.be.true;
+  });
+
+  it('error returns the correct statusCode', () => {
+    const statusCode = 501;
+    const resp = response.error(statusCode, {});
+    expect(resp.statusCode).to.equal(statusCode);
+  });
+
+  it('error returns the correct headers', () => {
+    const headers = {'traceId': '1122334455'};
+    const statusCode = 500;
+    const resp = response.error(statusCode, headers);
+    expect(resp.headers.hasOwnProperty('traceId')).to.be.true;
+  });
 });
